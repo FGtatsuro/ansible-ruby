9e06868011d7045b014dbe766c65dde40b8f41d1
FGtatsuro <fujiistorage@gmail.com>
2016-03-10 12:09:13 +0900

    Set GEM_PATH to Specinfra config.
    Or, Serverspec can't find gems Ansible playbooks install.

--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
+# In default, Serverspec uses the context specified environment variables are clear.
+# Ref. https://github.com/mizzy/specinfra/blob/master/lib/specinfra/backend/exec.rb#L128
+set :env, :GEM_PATH => ENV['GEM_PATH']
+
 describe package('ruby'), :if => os[:family] == 'darwin' do
   it { should be_installed.by('homebrew') }
 end
